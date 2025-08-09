d4c1aa4ce8ddeb00fb8438e228c615d3118d8308
ezdin <ezedinawol130@gmail.com>
2025-08-10 00:55:07 +0300

    implementing the leave request model

--- src/main/java/com/payroll/payroll_system/model/LeaveRequest.java
+++ src/main/java/com/payroll/payroll_system/model/LeaveRequest.java
@@ -1,4 +1,36 @@
 package com.payroll.payroll_system.model;
 
+import com.payroll.payroll_system.enums.LeaveType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LeaveRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = "leave type is required")
+    @Enumerated(EnumType.STRING)
+    private LeaveType leaveType;
+    @NotNull(message = "start date is required")
+    private LocalDate startDate;
+    @NotNull(message = "end date is required")
+    private LocalDate endDate;
+    @NotNull(message = "leave status is required")
+    @Enumerated(EnumType.STRING)
+    private LeaveType status;
+    @NotNull(message = "leave status is required")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emp_id")
+    private Employee empId;
 }
