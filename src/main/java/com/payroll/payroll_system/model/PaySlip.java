662a2b1d3a49ae18a9b1464a15f1f4728a446a31
ezdin <ezedinawol130@gmail.com>
2025-08-10 00:55:49 +0300

    implementing the pay slip  model

--- src/main/java/com/payroll/payroll_system/model/PaySlip.java
+++ src/main/java/com/payroll/payroll_system/model/PaySlip.java
@@ -1,4 +1,33 @@
 package com.payroll.payroll_system.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaySlip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = "gross salary is required")
+    private double grossSalary;
+    //'ToDo: make sure the deduction and allowances are based on employee position '
+    @NotNull(message = "total deduction is required")
+    private double totalDeduction;
+    @NotNull(message = "total allowance is required")
+    private double totalAllowance;
+    @NotNull(message = "net salary is required")
+    private double netSalary;
+    @NotNull(message = "employee id is required")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emp_id")
+    private Employee empId;
+
 }
