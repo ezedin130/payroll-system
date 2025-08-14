2806026274b154783c65d58e2cce8d832599892c
ezdin <ezedinawol130@gmail.com>
2025-08-14 08:49:14 +0300

    controller service for the user

---
+++ src/main/java/com/payroll/payroll_system/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.payroll.payroll_system.controller;
+
+import com.payroll.payroll_system.dto.UserDto.UserInDto;
+import com.payroll.payroll_system.dto.UserDto.UserOutDto;
+import com.payroll.payroll_system.mapper.UserMapper;
+import com.payroll.payroll_system.model.User;
+import com.payroll.payroll_system.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService service;
+    private final UserMapper mapper;
+
+    @PostMapping("/create-user")
+    public ResponseEntity<UserOutDto> createUser(@RequestBody UserInDto dto){
+        UserOutDto result = service.createUser(dto);
+        return ResponseEntity.ok(result);
+    }
+    @GetMapping("/get-all-users")
+    public ResponseEntity<List<UserOutDto>> getAllUsers(){
+        List<UserOutDto> result = service.getAllUsers();
+        return ResponseEntity.ok(result);
+    }
+    @GetMapping("/get-user-by-id/{id}")
+    public ResponseEntity<UserOutDto> getUserById(@PathVariable Long id){
+        User result = service.findUserById(id);
+        UserOutDto dto = mapper.toDto(result);
+        return ResponseEntity.ok(dto);
+    }
+}
