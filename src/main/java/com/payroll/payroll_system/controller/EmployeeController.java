e344c920ca6c2ce6e136f200a8110c5cab15bcf7
ezdin <ezedinawol130@gmail.com>
2025-08-14 08:47:41 +0300

    controller service for the employee

---
+++ src/main/java/com/payroll/payroll_system/controller/EmployeeController.java
@@ -0,0 +1,39 @@
+package com.payroll.payroll_system.controller;
+
+import com.payroll.payroll_system.dto.EmployeeDto.EmployeeInDto;
+import com.payroll.payroll_system.dto.EmployeeDto.EmployeeOutDto;
+import com.payroll.payroll_system.mapper.EmployeeMapper;
+import com.payroll.payroll_system.model.Employee;
+import com.payroll.payroll_system.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+@Validated
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService service;
+    private final EmployeeMapper mapper;
+
+    @PostMapping("/create-employee")
+    public ResponseEntity<EmployeeOutDto> createEmployee(@RequestBody EmployeeInDto dto){
+        EmployeeOutDto result = service.createEmployee(dto);
+        return ResponseEntity.ok(result);
+    }
+    @GetMapping("/get-all-employees")
+    public ResponseEntity<List<EmployeeOutDto>> getAllEmployees(){
+        List<EmployeeOutDto> result = service.getAllEmployees();
+        return ResponseEntity.ok(result);
+    }
+    @GetMapping("/get-employee-by-id/{id}")
+    public ResponseEntity<EmployeeOutDto> getEmployeeById(@PathVariable Long id){
+        Employee empl = service.searchEmployeeById(id);
+        EmployeeOutDto dto = mapper.toDto(empl);
+        return ResponseEntity.ok(dto);
+    }
+}
