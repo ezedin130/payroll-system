2e4fc18fc9c6b7201fb4eb299388542f66d1a973
ezdin <ezedinawol130@gmail.com>
2025-08-16 11:46:54 +0300

    remove other field , remaining the employee id , others are calculated , the user is not suppose to put them in

--- src/main/java/com/payroll/payroll_system/dto/PaySlipDto/PaySlipInDto.java
+++ src/main/java/com/payroll/payroll_system/dto/PaySlipDto/PaySlipInDto.java
@@ -4,9 +4,5 @@ import lombok.Data;
 
 @Data
 public class PaySlipInDto {
-    private double grossSalary;
-    private double totalDeduction;
-    private double totalAllowance;
-    private double netSalary;
     private Long empId;
 }
