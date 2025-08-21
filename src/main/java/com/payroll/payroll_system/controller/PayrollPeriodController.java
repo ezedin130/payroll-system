783123c9a173c5eed2376f92df1142b6ebc84fa2
ezdin <ezedinawol130@gmail.com>
2025-08-21 12:21:20 +0300

    updated controller that fits the payroll period service

--- src/main/java/com/payroll/payroll_system/controller/PayrollPeriodController.java
+++ src/main/java/com/payroll/payroll_system/controller/PayrollPeriodController.java
@@ -2,14 +2,14 @@ package com.payroll.payroll_system.controller;
 
 import com.payroll.payroll_system.dto.PayrollPeriodDto.PayrollPeriodInDto;
 import com.payroll.payroll_system.dto.PayrollPeriodDto.PayrollPeriodOutDto;
+import com.payroll.payroll_system.mapper.PayrollPeriodMapper;
+import com.payroll.payroll_system.model.Employee;
+import com.payroll.payroll_system.model.PayrollPeriod;
 import com.payroll.payroll_system.service.PayrollPeriodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,15 +18,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PayrollPeriodController {
     private final PayrollPeriodService service;
+    private final PayrollPeriodMapper mapper;
 
-    @PostMapping("payroll")
-    public ResponseEntity<PayrollPeriodOutDto> createPayroll(@RequestBody PayrollPeriodInDto dto){
-        PayrollPeriodOutDto result = service.createPayroll(dto);
-        return ResponseEntity.ok(result);
+    @PostMapping("/run")
+    public ResponseEntity<String> runPayroll(){
+        service.runPayroll();
+        return ResponseEntity.ok("Payroll has been processed successfully");
     }
     @GetMapping("payroll")
     public ResponseEntity<List<PayrollPeriodOutDto>> getAllPayrolls(){
         List<PayrollPeriodOutDto> result = service.getAllPayrolls();
         return ResponseEntity.ok(result);
+    }
+    @GetMapping("/{id")
+    public ResponseEntity<PayrollPeriodOutDto> getPayroll(@PathVariable Long id){
+        PayrollPeriod payroll = service.getPayrollById(id);
+        PayrollPeriodOutDto dto = mapper.toDto(payroll);
+        return ResponseEntity.ok(dto);
     }
 }
