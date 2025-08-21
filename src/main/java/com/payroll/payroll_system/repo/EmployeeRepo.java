7051ac93d97abe79b6512f620d41c4f8f5e9308f
ezdin <ezedinawol130@gmail.com>
2025-08-21 12:20:14 +0300

    new method that allows employee to be found using their status

--- src/main/java/com/payroll/payroll_system/repo/EmployeeRepo.java
+++ src/main/java/com/payroll/payroll_system/repo/EmployeeRepo.java
@@ -1,9 +1,13 @@
 package com.payroll.payroll_system.repo;
 
+import com.payroll.payroll_system.enums.status.EmployeeStatus;
 import com.payroll.payroll_system.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+    List<Employee> findByStatus(EmployeeStatus employeeStatus);
 }
