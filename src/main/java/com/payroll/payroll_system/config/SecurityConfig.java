2f32a1eecbbf08700b020944cd9a1574ed4561c9
ezdin <ezedinawol130@gmail.com>
2025-08-28 17:55:12 +0300

    configuration for the token based authentication

--- src/main/java/com/payroll/payroll_system/config/SecurityConfig.java
+++ src/main/java/com/payroll/payroll_system/config/SecurityConfig.java
@@ -1,8 +1,15 @@
 package com.payroll.payroll_system.config;
 
+import com.payroll.payroll_system.filter.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,8 +25,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
-
+    @Autowired
+    private final JwtFilter jwtFilter;
+    @Autowired
+    private final UserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
@@ -29,22 +40,19 @@ public class SecurityConfig {
                 // .formLogin(Customizer.withDefaults())//for browser
                 .httpBasic(Customizer.withDefaults())//for postman,...
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("1122"))
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
     }
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authManager(AuthenticationConfiguration config)throws Exception{
+        return  config.getAuthenticationManager();
     }
 }
