5a3e930c5c27e95b2210bc5bfe9def1dd0d24b62
ezdin <ezedinawol130@gmail.com>
2025-08-30 23:07:34 +0300

    dto used for authentication responses

---
+++ src/main/java/com/payroll/payroll_system/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.payroll.payroll_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String accessToken;
+    private String username;
+    private String role;
+}
